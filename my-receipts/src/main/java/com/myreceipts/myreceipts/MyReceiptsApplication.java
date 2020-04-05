24589e31507c0501668b1fba0958dc80fd5ed3d9
Venko Stojanov <venkostojanov@hotmail.com>
2020-04-05 18:19:29 +0200

    Initial commit

---
+++ my-receipts/src/main/java/com/myreceipts/myreceipts/MyReceiptsApplication.java
@@ -0,0 +1,13 @@
+package com.myreceipts.myreceipts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReceiptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyReceiptsApplication.class, args);
+    }
+
+}
---
+++ my-receipts/src/test/java/com/myreceipts/myreceipts/MyReceiptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myreceipts.myreceipts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyReceiptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
